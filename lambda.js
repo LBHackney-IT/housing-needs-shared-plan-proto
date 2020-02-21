3b90502ade9cb92f8c8656aff59a2780f3915ae3
Ben Pirt <ben@pirt.co.uk>
2020-02-21 17:40:48 +0000

    Basic API implementation

---
+++ lambda.js
@@ -0,0 +1,8 @@
+'use strict';
+const dbConfig = require('./lib/DynamoDbConfig')(process.env);
+const dbConn = require('./lib/DynamoDbConnection')(dbConfig);
+const dbGateway = require('./lib/gateways/Dynamo')(dbConn);
+const useCases = require('./lib/use-cases')({ dbGateway });
+const handlers = require('./lib/handlers')(useCases);
+
+module.exports = handlers;
