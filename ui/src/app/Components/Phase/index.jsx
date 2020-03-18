5f83f49a4a5d203c146af8fef01a42cee42e234d
Ben Pirt <ben@pirt.co.uk>
2020-03-18 15:10:08 +0000

    Update the feedback form link

--- ui/src/app/Components/Phase/index.jsx
+++ ui/src/app/Components/Phase/index.jsx
@@ -10,7 +10,7 @@ export default class Phase extends Component {
           </strong>
           <span className="govuk-phase-banner__text">
             This is a new service - your{' '}
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLSdgiT3dktz647tu0T1MMAmoQwI0CvdIdIR6zQPB7Cyo0-mIqg/viewform">
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSeOce8D_4boLCmEu18J1TVrNVDIuRAXThovasPII7EwHgHgeg/viewform">
               feedback
             </a>{' '}
             will help us to improve it.
